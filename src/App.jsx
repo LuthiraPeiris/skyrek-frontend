e4fcbbf27e0619ac490564401c2260fa11073634
Luthira Peiris <22cse0369@ms.sab.ac.lk>
2025-10-21 15:42:28 +0530

    Day 11 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import ProductCard from "./components/productCard";
+import Header from "./components/header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <ProductCard
-        name="Laptop"
-        image="https://picsum.photos/id/1/200/300"
-        price="100,000.00"
-      />
-      <ProductCard
-        name="Tab"
-        image="https://picsum.photos/id/2/200/300"
-        price="80,000.00"
-      />
-      <ProductCard
-        name="Phone"
-        image="https://picsum.photos/id/3/200/300"
-        price="75,000.00"
-      />
-    </>
+    <div className="border w-[600px] h-[600px] bg-gray-500">
+      <div className="w-[500px] h-[500px] bg-yellow-100 flex flex-col items-center justify-center">
+        <div className="w-[100px] h-[100px] bg-blue-600"></div>
+        <div className="w-[100px] h-[100px] bg-red-600 fixed left-[550px] top-[550px]"></div>
+        <div className="w-[100px] h-[100px] bg-green-600"></div>
+        <div className="w-[100px] h-[100px] absolute bg-pink-600"></div>
+      </div>
+    </div>
   );
 }
 
